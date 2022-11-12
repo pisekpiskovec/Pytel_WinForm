783ce23d9ccd2690854ed63509c8075dfed0e60e
Písek Pískovec <pisekpiskovec@gmail.com>
2022-11-12 09:48:05 +0100

    UI infterface yay \o/

--- Pytel_WinForm/Form1.Designer.cs
+++ Pytel_WinForm/Form1.Designer.cs
@@ -28,13 +28,219 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.tssbOpen = new System.Windows.Forms.ToolStripSplitButton();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsbQueue = new System.Windows.Forms.ToolStripButton();
+            this.tsbPrevious = new System.Windows.Forms.ToolStripButton();
+            this.tsb10SecBack = new System.Windows.Forms.ToolStripButton();
+            this.tsbPlay = new System.Windows.Forms.ToolStripButton();
+            this.tsbPause = new System.Windows.Forms.ToolStripButton();
+            this.tsbStop = new System.Windows.Forms.ToolStripButton();
+            this.tsb10SecForward = new System.Windows.Forms.ToolStripButton();
+            this.tsbNext = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tssbOpen,
+            this.tsbQueue,
+            this.toolStripSeparator1,
+            this.tsbPrevious,
+            this.tsb10SecBack,
+            this.tsbPlay,
+            this.tsbPause,
+            this.tsbStop,
+            this.tsb10SecForward,
+            this.tsbNext,
+            this.toolStripSeparator2,
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(66, 22);
+            this.toolStripLabel1.Text = "00:00/00:00";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Location = new System.Drawing.Point(12, 393);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(385, 45);
+            this.trackBar1.TabIndex = 1;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar2.Location = new System.Drawing.Point(403, 393);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(385, 45);
+            this.trackBar2.TabIndex = 2;
+            // 
+            // tssbOpen
+            // 
+            this.tssbOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tssbOpen.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.openFolderToolStripMenuItem});
+            this.tssbOpen.Image = global::Pytel_WinForm.Properties.Resources.opened_folder_96px;
+            this.tssbOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tssbOpen.Name = "tssbOpen";
+            this.tssbOpen.Size = new System.Drawing.Size(32, 22);
+            this.tssbOpen.Text = "Open file";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.openToolStripMenuItem.Text = "Open file";
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.openFolderToolStripMenuItem.Text = "Open folder";
+            // 
+            // tsbQueue
+            // 
+            this.tsbQueue.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbQueue.Image = global::Pytel_WinForm.Properties.Resources.video_playlist_96px;
+            this.tsbQueue.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbQueue.Name = "tsbQueue";
+            this.tsbQueue.Size = new System.Drawing.Size(23, 22);
+            this.tsbQueue.Text = "Queue";
+            // 
+            // tsbPrevious
+            // 
+            this.tsbPrevious.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbPrevious.Image = global::Pytel_WinForm.Properties.Resources.skip_to_start_96px;
+            this.tsbPrevious.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbPrevious.Name = "tsbPrevious";
+            this.tsbPrevious.Size = new System.Drawing.Size(23, 22);
+            this.tsbPrevious.Text = "Previous";
+            // 
+            // tsb10SecBack
+            // 
+            this.tsb10SecBack.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsb10SecBack.Image = global::Pytel_WinForm.Properties.Resources.rewind_96px;
+            this.tsb10SecBack.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsb10SecBack.Name = "tsb10SecBack";
+            this.tsb10SecBack.Size = new System.Drawing.Size(23, 22);
+            this.tsb10SecBack.Text = "Seek back";
+            // 
+            // tsbPlay
+            // 
+            this.tsbPlay.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbPlay.Image = global::Pytel_WinForm.Properties.Resources.play_96px;
+            this.tsbPlay.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbPlay.Name = "tsbPlay";
+            this.tsbPlay.Size = new System.Drawing.Size(23, 22);
+            this.tsbPlay.Text = "Play";
+            // 
+            // tsbPause
+            // 
+            this.tsbPause.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbPause.Image = global::Pytel_WinForm.Properties.Resources.pause_96px;
+            this.tsbPause.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbPause.Name = "tsbPause";
+            this.tsbPause.Size = new System.Drawing.Size(23, 22);
+            this.tsbPause.Text = "Pause";
+            // 
+            // tsbStop
+            // 
+            this.tsbStop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbStop.Image = global::Pytel_WinForm.Properties.Resources.stop_96px;
+            this.tsbStop.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbStop.Name = "tsbStop";
+            this.tsbStop.Size = new System.Drawing.Size(23, 22);
+            this.tsbStop.Text = "Stop";
+            // 
+            // tsb10SecForward
+            // 
+            this.tsb10SecForward.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsb10SecForward.Image = global::Pytel_WinForm.Properties.Resources.fast_forward_96px;
+            this.tsb10SecForward.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsb10SecForward.Name = "tsb10SecForward";
+            this.tsb10SecForward.Size = new System.Drawing.Size(23, 22);
+            this.tsb10SecForward.Text = "Seek forward";
+            // 
+            // tsbNext
+            // 
+            this.tsbNext.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbNext.Image = global::Pytel_WinForm.Properties.Resources.end_96px;
+            this.tsbNext.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbNext.Name = "tsbNext";
+            this.tsbNext.Size = new System.Drawing.Size(23, 22);
+            this.tsbNext.Text = "Next";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.ToolStripSplitButton tssbOpen;
+        private System.Windows.Forms.ToolStripButton tsbQueue;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton tsbPrevious;
+        private System.Windows.Forms.ToolStripButton tsb10SecBack;
+        private System.Windows.Forms.ToolStripButton tsbPlay;
+        private System.Windows.Forms.ToolStripButton tsbPause;
+        private System.Windows.Forms.ToolStripButton tsbStop;
+        private System.Windows.Forms.ToolStripButton tsb10SecForward;
+        private System.Windows.Forms.ToolStripButton tsbNext;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
     }
 }
 
